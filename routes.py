c6f1833fa738ffb8d2aa192210c59a20bdee1f7c
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2026-03-02 19:42:01 +0530

    dockerized app and db , basic dashboard done

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
-from flask import request, jsonify
-from services import process_event
+from flask import request, jsonify, render_template
+from services import process_event, get_events
+import json
+
 
 def register_routes(app):
 
@@ -11,7 +13,28 @@ def register_routes(app):
     def webhook():
         event_type = request.headers.get("X-GitHub-Event")
         payload = request.json
-
+        event_type = request.headers.get("X-GitHub-Event")
+        payload = request.json
         process_event(event_type, payload)
 
         return jsonify({"status": "received"}), 200
+    
+
+    
+        # print("Event Type:", event_type)
+        # print("Payload:")
+        # print(json.dumps(payload, indent=2))
+        # process_event(event_type, payload)
+
+            
+    # polling api 
+    @app.route("/events", methods=["GET"])
+    def get_event():
+        return jsonify(get_events())
+    
+    
+    @app.route("/dashboard")    
+    def dashboard():
+        return render_template("dashboard.html")
+    
+    
--- services.py
+++ services.py
@@ -1,3 +1,74 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+
+# ---- Mongo Setup ----
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
+
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
+collection = db[MONGO_COLLECTION]
+
+
+# ---- Business Logic ----
 def process_event(event_type, payload):
-    print("Event:", event_type)
-    print("Payload received")
+
+    if event_type == "push":
+
+        author = payload["pusher"]["name"]
+        to_branch = payload["ref"].split("/")[-1]
+        timestamp = payload["head_commit"]["timestamp"]
+
+        document = {
+            "event_type": "push",
+            "author": author,
+            "from_branch": None,
+            "to_branch": to_branch,
+            "timestamp": datetime.fromisoformat(timestamp)
+        }
+
+        collection.insert_one(document)
+
+    elif event_type == "pull_request":
+
+        action = payload.get("action")
+
+        # Brownie point: merge detection
+        if action == "closed" and payload["pull_request"]["merged"]:
+
+            author = payload["pull_request"]["user"]["login"]
+            from_branch = payload["pull_request"]["head"]["ref"]
+            to_branch = payload["pull_request"]["base"]["ref"]
+            timestamp = payload["pull_request"]["merged_at"]
+
+            document = {
+                "event_type": "merge",
+                "author": author,
+                "from_branch": from_branch,
+                "to_branch": to_branch,
+                "timestamp": datetime.fromisoformat(timestamp)
+            }
+
+            collection.insert_one(document)
+
+def get_events():
+    documents = collection.find().sort("timestamp", -1)
+
+    result = []
+
+    for doc in documents:
+        if doc["event_type"] == "push":
+            message = f'{doc["author"]} pushed to {doc["to_branch"]} on {doc["timestamp"]}'
+        elif doc["event_type"] == "merge":
+            message = f'{doc["author"]} merged branch {doc["from_branch"]} to {doc["to_branch"]} on {doc["timestamp"]}'
+        else:
+            continue
+
+        result.append(message)
+
+    return result
